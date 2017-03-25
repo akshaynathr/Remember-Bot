f848d4e59af039684e63190cd47e91a35ed79746
Akshaynath R <akshaynathr@gmail.com>
2017-03-25 12:05:16 +0530

    generic template

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 
 import requests
 from flask import Flask, request
+from wiki import *
 
 app = Flask(__name__)
 
@@ -41,6 +42,14 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
 
 #                    send_message(sender_id, message_text)
+
+                    msg_lst=message_text.split(" ")
+                    if msg_lst[0].lower()=="wiki":
+                        l=len(msg_lst[0])
+                        search=message_text[l:]
+                        result=search_wiki(search) 
+
+
                     send_message(sender_id, "got it, thanks!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
@@ -65,14 +74,54 @@ def send_message(recipient_id, message_text):
     headers = {
         "Content-Type": "application/json"
     }
-    data = json.dumps({
-        "recipient": {
+    data2=json.dumps({
+      "recipient": {
             "id": recipient_id
         },
-        "message": {
-            "text": message_text
-        }
-    })
+   
+        "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Welcome to Peter\'s Hats",
+            "image_url":"https://petersfancybrownhats.com/company_image.png",
+            "subtitle":"We\'ve got the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+              "messenger_extensions": true,
+              "webview_height_ratio": "tall",
+              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersfancybrownhats.com",
+                "title":"View Website"
+              },{
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
+  }
+
+        })
+    # data = json.dumps({
+    #     "recipient": {
+    #         "id": recipient_id
+    #     },
+    #     "message": {
+    #         "text": message_text
+    #     }
+    # })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
