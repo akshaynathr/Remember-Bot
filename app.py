a06a91cbd1824e96053e56ce813d723d0841447b
Akshaynath R <akshaynathr@gmail.com>
2017-03-25 18:53:25 +0530

    wiki functions working

--- app.py
+++ app.py
@@ -41,16 +41,31 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-#                    send_message(sender_id, message_text)
+                     
+                    msg_lst=message_text.split(" ")
+                    if msg_lst[0].lower()=="wiki":
+                        l=len(msg_lst[0])
+                        if len(message_text)==l:
+                            send_message(sender_id,"You specified nothing to search. Type 'Wiki search_term' ")
+                        else:
+                            search=message_text[l:]
+                            result=search_wiki(search) 
+                            article=get_results_wiki(result[0])
 
-                    # msg_lst=message_text.split(" ")
-                    # if msg_lst[0].lower()=="wiki":
-                    #     l=len(msg_lst[0])
-                    #     search=message_text[l:]
-                    #     result=search_wiki(search) 
+
+                            heading = article.heading
+                            image=article.image
+                            url=article.url
+                            fall_back_url="https://en.wikipedia.org/"
+                            data=generic_template(url=url,img_url=image,title=heading,sub_title=None,fall_back_url=fall_back_url,btn_url=url,btn_title="Go to wiki",recipient=sender_id)
+
+                            send_message(sender_id,None,data)
 
 
-                    send_message(sender_id, "got it, thanks!")
+
+                            #send_message(sender_id, article.summary[:600])
+                    else:
+                        send_message(sender_id,"TEST")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -64,65 +79,33 @@ def webhook():
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
+def send_message(recipient_id, message_text,data=None):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": "EAAPJyQPXUXMBADCpmjYyv3NDVJHEsVzTNLuzZBnAGj0BOsVZCu14fV9En4LCQPyPE08lMlWw3v1ggcKGWXb3hbHwZB2RHuoOtx82VB9bjItLBH9fZCEor7sLdGRA1Lb44xur8EHgDVYdX4bmutaSGFo2b4xOOoZAyoPVqdy2yKAZDZD"
+        "access_token": "EAAPJyQPXUXMBAHcfCZChSIaJzaNahe04jsGkBSdlhTz637ZA3rUmBcUXJ6cQKbOVmLUAfSLT4hA5zY5ZCfi2Lv6uk7lTxKM5OT29nHMhrNJSOq0lzZCSeftHAPnt7CPdiTCZAsVi4Vdnc4li83Ml0MbloINZCJmFZCItKD9e7IYZCwZDZD"
     }
     headers = {
         "Content-Type": "application/json"
     }
-    data2=json.dumps({
-      "recipient": {
+    
+    #data=generic_template()
+    data2 = json.dumps({
+        "recipient": {
             "id": recipient_id
         },
-   
-        "message":{
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Welcome to Peter\'s Hats",
-            "image_url":"http://www.villagehatshop.com/photos/product/standard/4511390S61417/mens-hats/mj-panama-straw-outback-hat.jpg",#"https://petersfancybrownhats.com/company_image.png",
-            "subtitle":"We\'ve got the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-              "messenger_extensions": True,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }              
-            ]      
-          }
-        ]
-      }
-    }
-  }
+      
+        "message": {
+            "text": message_text
+        }
+    })
+    if data==None:
+        data=data2
 
-        })
-    # data = json.dumps({
-    #     "recipient": {
-    #         "id": recipient_id
-    #     },
-    #     "message": {
-    #         "text": message_text
-    #     }
-    # })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data2)
+
+    send_writing(recipient_id)
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
@@ -133,3 +116,117 @@ def log(message):  # simple wrapper for logging to stdout on heroku
     sys.stdout.flush()
 
 
+
+
+
+
+
+
+
+def generic_template(url,img_url,title,sub_title,fall_back_url,btn_url,btn_title,recipient):
+    #   data2=json.dumps({
+    #     "recipient": {
+    #           "id": recipient_id
+    #       },
+     
+    #       "message":{
+    #   "attachment":{
+    #     "type":"template",
+    #     "payload":{
+    #       "template_type":"generic",
+    #       "elements":[
+    #          {
+    #           "title":"Welcome to Peter\'s Hats",
+    #           "image_url":"http://www.villagehatshop.com/photos/product/standard/4511390S61417/mens-hats/mj-panama-straw-outback-hat.jpg",#"https://petersfancybrownhats.com/company_image.png",
+    #           "subtitle":"We\'ve got the right hat for everyone.",
+    #           "default_action": {
+    #             "type": "web_url",
+    #             "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+    #             "messenger_extensions": True,
+    #             "webview_height_ratio": "tall",
+    #             "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+    #           },
+    #           "buttons":[
+    #             {
+    #               "type":"web_url",
+    #               "url":"https://petersfancybrownhats.com",
+    #               "title":"View Website"
+    #             },{
+    #               "type":"postback",
+    #               "title":"Start Chatting",
+    #               "payload":"DEVELOPER_DEFINED_PAYLOAD"
+    #             }              
+    #           ]      
+    #         }
+    #       ]
+    #     }
+    #   }
+    # }
+
+    #       })
+
+    data2=json.dumps({
+        "recipient": {
+              "id": recipient
+          },
+     
+          "message":{
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":title,
+              "image_url":img_url,#"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":sub_title,
+              "default_action": {
+                "type": "web_url",
+                "url": url,
+                "messenger_extensions": True,
+                "webview_height_ratio": "tall",
+                "fallback_url": fall_back_url
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":btn_url,
+                  "title":"Read more"
+                }          
+              ]      
+            }
+          ]
+        }
+      }
+    }
+
+          })
+    print (data2)
+    return data2
+
+
+
+
+
+
+
+def send_writing(recipient):
+
+    data=json.dumps({
+    "recipient":{
+          "id":recipient
+    },
+    "sender_action":"typing_on"  
+})  
+
+    params = {
+        "access_token": "EAAPJyQPXUXMBAHcfCZChSIaJzaNahe04jsGkBSdlhTz637ZA3rUmBcUXJ6cQKbOVmLUAfSLT4hA5zY5ZCfi2Lv6uk7lTxKM5OT29nHMhrNJSOq0lzZCSeftHAPnt7CPdiTCZAsVi4Vdnc4li83Ml0MbloINZCJmFZCItKD9e7IYZCwZDZD"
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
